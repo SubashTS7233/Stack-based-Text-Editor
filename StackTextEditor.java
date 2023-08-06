2a035c3abb98718665228187923358ac6e0410b6
SubashTS7233 <138566800+SubashTS7233@users.noreply.github.com>
2023-08-06 14:29:17 +0530

    Update StackTextEditor.java

--- StackTextEditor.java
+++ StackTextEditor.java
@@ -10,85 +10,52 @@ public class StackTextEditor {
         undoStack = new Stack<>();
         redoStack = new Stack<>();
     }
-
     public void insertText(String text) {
-        // Save the current state for undo
         undoStack.push(currentText.toString());
-
-        // Perform the insert operation
         currentText.append(text);
-
-        // Clear the redo stack since the redo history is invalid after new edits
         redoStack.clear();
     }
-
     public void deleteCharacters(int count) {
         if (count <= 0 || count > currentText.length())
             return;
-
-        // Save the current state for undo
         undoStack.push(currentText.toString());
-
-        // Perform the delete operation
         int newLength = currentText.length() - count;
         currentText.setLength(newLength);
-
-        // Clear the redo stack since the redo history is invalid after new edits
         redoStack.clear();
     }
-
     public boolean canUndo() {
         return !undoStack.isEmpty();
     }
-
     public void undo() {
         if (canUndo()) {
-            // Save the current state for redo
             redoStack.push(currentText.toString());
-
-            // Retrieve the previous state and set it as the current text
             String previousState = undoStack.pop();
             currentText = new StringBuilder(previousState);
         }
     }
-
     public boolean canRedo() {
         return !redoStack.isEmpty();
     }
-
     public void redo() {
         if (canRedo()) {
-            // Save the current state for undo
             undoStack.push(currentText.toString());
-
-            // Retrieve the next state and set it as the current text
             String nextState = redoStack.pop();
             currentText = new StringBuilder(nextState);
         }
     }
-
     public String getCurrentText() {
         return currentText.toString();
     }
-
     public static void main(String[] args) {
         StackTextEditor editor = new StackTextEditor();
-
         editor.insertText("Hello ");
         editor.insertText("World!");
-
-        System.out.println(editor.getCurrentText()); // Output: "Hello World!"
-
+        System.out.println(editor.getCurrentText()); 
         editor.deleteCharacters(6);
-
-        System.out.println(editor.getCurrentText()); // Output: "Hello "
-
+        System.out.println(editor.getCurrentText());
         editor.undo();
-
-        System.out.println(editor.getCurrentText()); // Output: "Hello World!"
-
+        System.out.println(editor.getCurrentText()); 
         editor.redo();
-
-        System.out.println(editor.getCurrentText()); // Output: "Hello "
+        System.out.println(editor.getCurrentText()); 
     }
 }
